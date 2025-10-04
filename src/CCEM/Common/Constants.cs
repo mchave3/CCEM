241023a2351e3b56793054497661215516b6eeb7
Mickaël CHAVE <mchave3@live.fr>
2025-10-04 02:26:01 +0200

    fix: Update directory paths and log file naming in Constants class

--- src/CCEM/Common/Constants.cs
+++ src/CCEM/Common/Constants.cs
@@ -2,8 +2,8 @@
 
 public static partial class Constants
 {
-    public static readonly string RootDirectoryPath = Path.Combine(PathHelper.GetAppDataFolderPath(), ProcessInfoHelper.ProductNameAndVersion);
+    public static readonly string RootDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "CCEM");
     public static readonly string LogDirectoryPath = Path.Combine(RootDirectoryPath, "Log");
-    public static readonly string LogFilePath = Path.Combine(LogDirectoryPath, "Log.txt");
+    public static readonly string LogFilePath = Path.Combine(LogDirectoryPath, "CCEM.log");
     public static readonly string AppConfigPath = Path.Combine(RootDirectoryPath, "AppConfig.json");
 }
