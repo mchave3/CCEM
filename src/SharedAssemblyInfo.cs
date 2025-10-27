a5e40dc489779acaec78ef183399527371c866ea
Mickaël CHAVE <mchave3@live.fr>
2025-10-27 16:48:35 +0100

    feat: Add SharedAssemblyInfo and Directory.Build.props files; update project files to include shared assembly info

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyProduct("CCEM")]
+[assembly: AssemblyDescription("Client Center for Endpoint Manager")]
+[assembly: AssemblyTitle("Client Center for Endpoint Manager")]
+[assembly: AssemblyDefaultAlias("CCEM")]
+[assembly: AssemblyCopyright("2025, Mickaël CHAVE")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: SupportedOSPlatform("windows10.0.19044")]
